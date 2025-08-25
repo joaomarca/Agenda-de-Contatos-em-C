da9db6ba094d486ba82bb332d1000b0be7984907
João Francisco Marca <joaomarca.dev@gmail.com>
2025-08-25 03:16:44 -0300

    Adicionada função de buscar contatos por nome.

--- main.c
+++ main.c
@@ -2,18 +2,31 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <ctype.h>
 
+void normalizarString(char *dest, const char *src);
 void adicionarContato(char nome[], char email[], int numero);
 void listarContatos();
-void buscarContato(char nome[]);
+void buscarContato(char nomeProcurado[]);
 void removerContato(char nome[]);
 void sair();
 
 int main() {
-    listarContatos();
+    buscarContato("    n    o M e   ");
     return 0;
 }
 
+
+void normalizarString(char *dest, const char *src) {
+    while (*src) {
+        if (!isspace((unsigned char)*src)) {
+            *dest = tolower((unsigned char)*src);
+            dest++;
+        }
+        src++;
+    }
+    *dest = '\0';
+}
 
 void adicionarContato(char nome[], char email[], int numero) {
     FILE *f = fopen("contatos.txt", "a+");
@@ -69,5 +82,54 @@ void listarContatos() {
         }
     }
 
+    fclose(f);
+}
+
+void buscarContato(char nomeProcurado[]) {
+    FILE *f = fopen("contatos.txt", "r");
+    if (f == NULL) {
+        printf("Erro ao ler arquivo contatos.txt");
+        fclose(f);
+        return;
+    }
+
+    struct stat file_info;
+    stat("contatos.txt", &file_info);
+
+    if (file_info.st_size == 0) {
+        printf("Agenda de contatos vazia!");
+        fclose(f);
+        return;
+    }
+
+    char buffer[150];
+    char nomeSanitizado[100];
+    char procuradoSanitizado[100];
+    int encontrado = 0;
+
+    normalizarString(procuradoSanitizado, nomeProcurado);
+
+    while (fgets(buffer, sizeof(buffer), f)) {
+        buffer[strcspn(buffer, "\n")] = 0;
+        char *nome = strtok(buffer, ",");
+
+        if (nome) {
+            normalizarString(nomeSanitizado, nome);
+
+            if (strcmp(nomeSanitizado, procuradoSanitizado) == 0) {
+                encontrado = 1;
+                char *email = strtok(NULL, ",");
+                char *numero = strtok(NULL, ",");
+
+                printf("\nNome: %s\n", nome);
+                printf("Email: %s\n", email);
+                printf("Numero: %s\n", numero);
+            }
+        }
+    }
+
+    if (!encontrado) {
+        printf("\nContato não encontrado!\n");
+    }
     fclose(f);
 }

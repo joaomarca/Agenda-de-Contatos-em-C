7ca8f0efd24b34a278c11d0e5166c954397e7a90
João Francisco Marca <joaomarca.dev@gmail.com>
2025-08-25 03:39:40 -0300

    Adicionado função para deletar contatos.

--- main.c
+++ main.c
@@ -8,11 +8,10 @@ void normalizarString(char *dest, const char *src);
 void adicionarContato(char nome[], char email[], int numero);
 void listarContatos();
 void buscarContato(char nomeProcurado[]);
-void removerContato(char nome[]);
+void removerContato(char nomeProcurado[]);
 void sair();
 
 int main() {
-    buscarContato("    n    o M e   ");
     return 0;
 }
 
@@ -31,7 +30,7 @@ void normalizarString(char *dest, const char *src) {
 void adicionarContato(char nome[], char email[], int numero) {
     FILE *f = fopen("contatos.txt", "a+");
     if (f == NULL) {
-        printf("Erro ao abrir arquivo contatos.txt");
+        printf("Erro ao abrir arquivo contatos.txt\n");
         fclose(f);
         return;
     }
@@ -53,7 +52,7 @@ void adicionarContato(char nome[], char email[], int numero) {
 void listarContatos() {
     FILE *f = fopen("contatos.txt", "r");
     if (f == NULL) {
-        printf("Erro ao ler arquivo contatos.txt");
+        printf("Erro ao ler arquivo contatos.txt\n");
         fclose(f);
         return;
     }
@@ -62,7 +61,7 @@ void listarContatos() {
     stat("contatos.txt", &file_info);
 
     if (file_info.st_size == 0) {
-        printf("Agenda de contatos vazia!");
+        printf("Agenda de contatos vazia!\n");
         fclose(f);
         return;
     }
@@ -88,7 +87,7 @@ void listarContatos() {
 void buscarContato(char nomeProcurado[]) {
     FILE *f = fopen("contatos.txt", "r");
     if (f == NULL) {
-        printf("Erro ao ler arquivo contatos.txt");
+        printf("Erro ao ler arquivo contatos.txt\n");
         fclose(f);
         return;
     }
@@ -97,7 +96,7 @@ void buscarContato(char nomeProcurado[]) {
     stat("contatos.txt", &file_info);
 
     if (file_info.st_size == 0) {
-        printf("Agenda de contatos vazia!");
+        printf("Agenda de contatos vazia!\n");
         fclose(f);
         return;
     }
@@ -132,4 +131,70 @@ void buscarContato(char nomeProcurado[]) {
         printf("\nContato não encontrado!\n");
     }
     fclose(f);
+}
+
+void removerContato(char nomeProcurado[]) {
+    FILE *f = fopen("contatos.txt", "r");
+    if (f == NULL) {
+        printf("Erro ao ler arquivo contatos.txt\n");
+        fclose(f);
+        return;
+    }
+
+    struct stat file_info;
+    stat("contatos.txt", &file_info);
+
+    if (file_info.st_size == 0) {
+        printf("Agenda de contatos vazia!\n");
+        fclose(f);
+        return;
+    }
+
+    FILE *temp = fopen("temp.txt", "w");
+    if (temp == NULL) {
+        printf("Erro ao criar arquivo temporário!\n");
+        fclose(f);
+        return;
+    }
+
+    char buffer[150];
+    char nomeSanitizado[100];
+    char procuradoSanitizado[100];
+    int encontrado = 0;
+
+    normalizarString(procuradoSanitizado, nomeProcurado);
+
+    while (fgets(buffer, sizeof(buffer), f)) {
+        buffer[strcspn(buffer, "\n")] = 0;
+        char linha[150];
+        strcpy(linha, buffer);
+
+        char *nome = strtok(buffer, ",");
+
+        if (nome) {
+            normalizarString(nomeSanitizado, nome);
+
+            if (strcmp(nomeSanitizado, procuradoSanitizado) == 0) {
+                encontrado++;
+                continue;
+            }
+        }
+
+        fprintf(temp, "%s\n", linha);
+    }
+
+    fclose(f);
+    fclose(temp);
+
+    remove("contatos.txt");
+    rename("temp.txt", "contatos.txt");
+
+    if (!encontrado) {
+        printf("\nContato não encontrado!\n");
+        return;
+    } else if (encontrado == 1) {
+        printf("Contato deletado com sucesso!\n");
+    } else {
+        printf("%d contatos deletados com sucesso!", encontrado);
+    }
 }
